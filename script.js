8d54339b70b1848a63581736168a3e4fa3ea5e7a
Quemmies3 <quemestree@gmail.com>
2022-06-21 14:02:04 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const Name = 'Olukemi Odedeyi';
+const Height = '5 ft 7';
+const Country = 'Nigeria';
+
+
+const displayName = document.getElementById('displayName');
+const displayHeight = document.getElementById('displayHeight');
+const displayCountry = document.getElementById('displayCountry');('displayName');
+
+displayName.innerHTML = `<p>Hello, my name is  ${Name}</p>`
+displayHeight.innerHTML = `<p>I am ${Height} tall</p>`
+displayCountry.innerHTML = `<p>I am from  ${Country}</p>`
